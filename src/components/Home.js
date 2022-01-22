54dd4c86d15d5237c20b1789507f7dc6470e21eb
mw-awais.niaz <awais.niaz@mwanmobile.org>
2022-01-22 15:31:19 +0500

    React query implementation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import { useQuery,useQueryClient } from 'react-query'
+import axios from 'axios';
 export default function Home() {
+  const client = useQueryClient()
+  const onSuccess = (data)=>{
+    console.log(data)
+  }
+  const onError = (err)=>{
+    console.log(err.message)
+  }
+  const {isLoading,data,refetch} = useQuery('repoData',()=>{
+    return axios.get('https://random-data-api.com/api/coffee/random_coffee')
+  },
+  {
+    onSuccess:onSuccess,
+    onError:onError,
+    enabled:false,
+    refetchOnWindowFocus:false,
+    cacheTime:100000000
+  }
+  )
   return (
     <div>
-      
+      {isLoading && <p>I am Loading</p>}
+      {data && <p>Final I get the data</p>}
+      <button onClick = {()=>{
+        // console.log(client.getQueryData('repoData'))
+        if(client.getQueryData('repoData') != undefined){
+          return client.getQueryData('repoData')?.data
+        }else{
+        refetch()}}}
+>Fetch data </button>
     </div>
+    
   )
 }

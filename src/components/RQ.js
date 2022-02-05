cc280f3566304596b2ffd5f117157f82403af44e
Awais Niaz <awais.niaz@mwanmobile.org>
2022-02-05 18:56:33 +0500

    react query

--- src/components/RQ.js
+++ src/components/RQ.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useMutation } from 'react-query'
+import axios from 'axios';
+
 
 export default function RQ() {
+  const mutation = useMutation(newTodo => {
+    return axios.post('/todos', newTodo)
+  })
   return (
     <div>
-      
+      {
+        mutation.isLoading && <p>Adding todo....</p>
+      }
+      {mutation.isError && <p>{mutation.error.message}</p>}
+
     </div>
   )
 }
